0698f45ca19e44c242aec15204d5a15502c8a126
Diego Melendez <diedu89@gmail.com>
2021-06-14 20:43:36 -0600

    feat: add utility function to render component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,17 +4,29 @@ import userEvent from "@testing-library/user-event";
 import { act } from "react-dom/test-utils";
 import { DraggerProps } from "antd/lib/upload";
 
-jest.mock('antd', () => {
-  const antd = jest.requireActual('antd');
+const elementRenderer = ({ onUploadComplete = () => {} }) => (
+  <div className="antd-local">
+    <App onUploadComplete={onUploadComplete} />
+  </div>
+);
+
+jest.mock("antd", () => {
+  const antd = jest.requireActual("antd");
   const { Upload } = antd;
   const { Dragger } = Upload;
 
   const MockedDragger = (props: DraggerProps) => {
-    return <Dragger {...props} customRequest={({ onSuccess }) => {
-      setTimeout(() => {
-        onSuccess('ok');
-      }, 0)
-    }} />;
+    console.log('rendering mock');
+    return (
+      <Dragger
+        {...props}
+        customRequest={({ onSuccess }) => {
+          setTimeout(() => {
+            onSuccess("ok");
+          }, 0);
+        }}
+      />
+    );
   };
 
   return { ...antd, Upload: { ...Upload, Dragger: MockedDragger } };
@@ -24,9 +36,7 @@ it("completes the image upload", async () => {
   const onUploadComplete = jest.fn();
   const flushPromises = () => new Promise(setImmediate);
 
-  const { getByTestId } = render(
-    <App onUploadComplete={onUploadComplete} />
-  );
+  const { getByTestId } = render(elementRenderer({ onUploadComplete }));
 
   const file = new File(["(⌐□_□)"], "chucknorris.png", { type: "image/png" });
 
